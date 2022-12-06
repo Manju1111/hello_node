5f0b7c25c264cae56f0de2711fa7df3b5a766a97
Manju1111 <awatimanju020@gmail.com>
2022-12-06 13:44:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser =require('body-parser');
+const PORT = 5002;
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended : true}))
+
+
+app.get('/helloworld',(req,res)=>{
+res.send('hello word on Web live')
+console.log('helloword get api call')
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`listining to port ${PORT}`)
+})
